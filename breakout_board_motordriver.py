5631f983eeed82802fcd8b4062b34cd42152bfdc
CompSciTeaching <stuart_thurgood@hotmail.com>
2017-07-20 17:47:31 +0100

    Add files via upload

---
+++ breakout_board_motordriver.py
@@ -0,0 +1,18 @@
+#Breakout Board Motor Controller
+from microbit import *
+motorA_FWD = pin8
+motorA_REV = pin12
+motorB_FWD = pin0
+motorB_REV = pin16
+while True:
+    motorA_REV.write_digital(1)
+    motorB_REV.write_digital(1)
+    sleep(1000)
+    motorA_REV.write_digital(0)
+    motorB_REV.write_digital(0)
+    sleep(1000)
+    motorA_FWD.write_digital(1)
+    motorB_FWD.write_digital(1)
+    sleep(1000)
+    motorA_FWD.write_digital(0)
+    motorB_FWD.write_digital(0)
